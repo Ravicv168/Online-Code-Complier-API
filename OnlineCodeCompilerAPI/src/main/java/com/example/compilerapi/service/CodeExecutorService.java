dae6d85f21c47e2c58777a784638f17b687f9f73
Admin <ravicv2000cv@gmail.com>
2024-12-28 16:41:25 +0530

    Updated the method in service class with proper comments.

--- OnlineCodeCompilerAPI/src/main/java/com/example/compilerapi/service/CodeExecutorService.java
+++ OnlineCodeCompilerAPI/src/main/java/com/example/compilerapi/service/CodeExecutorService.java
@@ -1,7 +1,6 @@
 package com.example.compilerapi.service;
 
 import java.io.*;
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
 
@@ -11,55 +10,60 @@ import com.example.compilerapi.model.CodeResponse;
 @Service
 public class CodeExecutorService {
 
+	// Method to execute the code based on the language
 	public CodeResponse executeCode(CodeRequest coderequest) {
 		String output = "";
 	    String error = "";
 	    boolean success = false;
 	    String language = coderequest.getLanguage();
+	    String message = "Execution Complete";
 		try {
 			switch (coderequest.getLanguage().toLowerCase()) {
             case "java":
                 output = executeJavaCode(coderequest.getCode());
                 success = true;
                 break;
-//            case "c":
-//                output = executeCCode(coderequest.getCode());
-//                break;
-//            case "cpp":
-//                output = executeCppCode(coderequest.getCode());
-//                success = true;
-//                break;
             case "python":
                 output = executePythonCode(coderequest.getCode());
                 success = true;
                 break;
             default:
                 output = "Unsupported language: " + coderequest.getLanguage();
+                message = "Execution failed";
                 break;
 			}
 		}catch (Exception e) {
 			error="Error Executing Code: "+ e.getMessage();
+			message = "Execution failed";
 		}
-		return new CodeResponse(output, error, success, language, "Execution Complete");
+		return new CodeResponse(output, error, success, language, message);
 	}
 	
+	// Method to execute Java code
 	private String executeJavaCode(String code) throws Exception{
+		
+		// Save Java code to a temporary file
 		File file = saveCodeToFile(code, ".java");
 		String classname = file.getName().replace(".java", "");
 		
+		// Compile Java code
 		if(!compileJavaCode(file)) {
 			return "Compilation failed";
 		}
-		
+		// Run Java code
 		return runJavaCode(classname);
 	}
 	
+	// Method to execute Java code
 	private String executePythonCode(String code) throws Exception {
+		// Save Python code to a temporary file
         File sourceFile = saveCodeToFile(code, ".py");
 
+        // Execute Python code
         return runPythonCode(sourceFile);
     }
 	
+	// Helper method to save code to a temporary file
 	private File saveCodeToFile(String code, String fileextension) throws IOException {
 		String classname = extractClassName(code);
 		File tempfile=new File(classname + fileextension);
@@ -69,15 +73,17 @@ public class CodeExecutorService {
         return tempfile;
 	}
 	
+	// Helper method to extract the class name.
 	private String extractClassName(String code) {
 	    int classIndex = code.indexOf("class ");
 	    if (classIndex != -1) {
 	        int endIndex = code.indexOf(" ", classIndex + 6);   
 	        return code.substring(classIndex + 6, endIndex != -1 ? endIndex : code.indexOf("\n", classIndex));
 	    }
-	    return "UnknownClass";  
+	    return "Temp";  
 	}
 	
+	// Helper method to compile Java code
 	private boolean compileJavaCode(File sourceFile) throws Exception {
         Process compileProcess = new ProcessBuilder("javac", sourceFile.getAbsolutePath()).start();
         
@@ -112,16 +118,19 @@ public class CodeExecutorService {
         return exitCode == 0;
     }
 	
+	// Helper method to run Java code
 	private String runJavaCode(String className) throws Exception {
         Process runProcess = new ProcessBuilder("java", className).start();
         return readProcessOutput(runProcess);
     }
 	
+	// Helper method to run Python code
 	private String runPythonCode(File sourceFile) throws Exception {
         Process process = new ProcessBuilder("python", sourceFile.getAbsolutePath()).start();
         return readProcessOutput(process);
     }
 	
+	 // Helper method to read output from the process
 	private String readProcessOutput(Process process) throws IOException {
         StringBuilder output = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
